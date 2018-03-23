94d9cb3a3aaae6656760c5cb1451ab731f7ad9fb
Sarah Campbell <scampbell.home@btinternet.com>
2018-03-23 19:31:40 +0000

    Added Set<Article> articles property to Editor class.

--- src/main/java/models/Editor.java
+++ src/main/java/models/Editor.java
@@ -1,12 +1,17 @@
 package models;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Editor extends Employee {
+
+    private Set<Article> articles;
 
     public Editor() {
     }
 
     public Editor(String firstName, String lastName) {
         super(firstName, lastName);
+        this.articles = new HashSet<>();
     }
-
 }
