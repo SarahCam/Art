58dd9e597ca43a6192d53968794681598ae5bef4
Sarah Campbell <scampbell.home@btinternet.com>
2018-03-23 18:30:23 +0000

    Added Article class properties and getters/setters.  Added TestJournal class TDD tests.

--- src/main/java/models/Article.java
+++ src/main/java/models/Article.java
@@ -1,9 +1,113 @@
 package models;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 public class Article {
 
     private int id;
+    private Calendar creationDate;
+    private GregorianCalendar publishedDate;
     private String headLine;
     private String lede;
+    private String story;
+    private String category;                                            // Convert this to an ENUM
+    private String status;                                              // Convert this to an ENUM
+    private Journalist journalist;
+    private Editor editor;
 
+    public Article() {
+    }
+
+    public Article(String headLine) {
+        this.creationDate = GregorianCalendar.getInstance();            // Get current date from computer
+        this.publishedDate = null;
+        this.headLine = headLine;
+        this.lede = null;
+        this.story = null;
+        this.status = "draft";                                          // Replace with ENUM
+        this.journalist = null;
+        this.editor = null;
+        this.category = null;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Calendar getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Calendar creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public GregorianCalendar getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(GregorianCalendar publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+
+    public String getHeadLine() {
+        return headLine;
+    }
+
+    public void setHeadLine(String headLine) {
+        this.headLine = headLine;
+    }
+
+    public String getLede() {
+        return lede;
+    }
+
+    public void setLede(String lede) {
+        this.lede = lede;
+    }
+
+    public String getStory() {
+        return story;
+    }
+
+    public void setStory(String story) {
+        this.story = story;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Journalist getJournalist() {
+        return journalist;
+    }
+
+    public void setJournalist(Journalist journalist) {
+        this.journalist = journalist;
+    }
+
+    public Editor getEditor() {
+        return editor;
+    }
+
+    public void setEditor(Editor editor) {
+        this.editor = editor;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
--- src/test/java/TestJournalist.java
+++ src/test/java/TestJournalist.java
@@ -1,2 +1,37 @@
+import models.Article;
+import models.Editor;
+import models.Journalist;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+
+
 public class TestJournalist {
+
+    private Journalist journalist;
+    private Article article;
+
+    @Before
+    public void setUp() throws Exception {
+        journalist = new Journalist("Suzy", "Smith");
+    }
+
+    @Test
+    public void canGetFirstName() {
+        assertEquals("Suzy", journalist.getFirstName());
+    }
+
+    @Test
+    public void canGetLastName() {
+        assertEquals("Smith", journalist.getLastName());
+    }
+
+    @Test
+    public void canCreateArticle() {
+        journalist.CreateArticle("Jurassic Escape");
+        assertEquals(1, journalist.getArticles().size());
+    }
 }
