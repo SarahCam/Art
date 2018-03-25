4d99474da5ef504cebb56eca862258e4421a1e84
Sarah Campbell <scampbell.home@btinternet.com>
2018-03-25 23:15:25 +0100

    Added hireDate and quitDate GregorianCalendar properties to Employee class.

--- src/main/java/models/Employee.java
+++ src/main/java/models/Employee.java
@@ -1,12 +1,15 @@
 package models;
 
 import java.util.ArrayList;
+import java.util.GregorianCalendar;
 
 public abstract class Employee {
 
     private int id;
     private String firstName;
     private String lastName;
+    private GregorianCalendar hireDate;
+    private GregorianCalendar quitDate;
 
     public Employee() {
     }
@@ -38,5 +41,21 @@ public abstract class Employee {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public GregorianCalendar getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(GregorianCalendar hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public GregorianCalendar getQuitDate() {
+        return quitDate;
+    }
+
+    public void setQuitDate(GregorianCalendar quitDate) {
+        this.quitDate = quitDate;
     }
 }
