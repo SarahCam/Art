c06ed9d6e00056236ea48b75c239556c87c41ad9
Sarah Campbell <scampbell.home@btinternet.com>
2018-03-25 08:51:39 +0100

    Added Article class tests.  Add a Set<Article> articles property to Editor class.

--- src/test/java/TestArticle.java
+++ src/test/java/TestArticle.java
@@ -1,2 +1,35 @@
+import models.Article;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import static org.junit.Assert.assertEquals;
+
 public class TestArticle {
+
+    private Article article;
+
+    @Before
+    public void setUp() throws Exception {
+        article = new Article("Dinosaur Rampage");
+    }
+
+    @Test
+    public void canGetHeadline() {
+        assertEquals("Dinosaur Rampage", article.getHeadLine());
+    }
+
+    @Test
+    public void canGetCreationDate() {
+        assertEquals(Calendar.getInstance(), article.getCreationDate());
+    }
+
+    @Test
+    public void canGetPublishedDate() {
+        GregorianCalendar testDate = new GregorianCalendar(18, 12, 9);
+        article.setPublishedDate(testDate);
+        assertEquals(testDate, article.getPublishedDate());
+    }
 }
